f8786f36b6cd8d274a02199d379bb33d113025a7
dir-FY24rioueda <rio,ueda.b122@dirbato.co.jp>
2024-04-09 09:26:32 +0900

    完了

---
+++ Sample/src/sample/ForSample.java
@@ -0,0 +1,15 @@
+package sample;
+
+import java.util.Iterator;
+
+public class ForSample {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+       for (var i = 1; i <= 10; i++) {
+           System.out.println(i);
+           
+       }
+    }
+
+}
---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Test {
+
+ // main メソッドはプログラムの最初に動作します。
+
+    public static void main(String[] args) {
+        /*コメントアウト
+        System.out.println("kiramex");
+        System.out.println("Java");
+        */
+        System.out.println("学習を始めましょう！");
+   
+        
+    }
+
+}
